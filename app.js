2756461e24ad5d6433b6d066e78b4f2dd2daf8ab
ChiragSuthar0 <85072436+ChiragSuthar0@users.noreply.github.com>
2021-06-19 17:19:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+var mysql = require('mysql');
+var cors = require('cors')
+const port = process.env.PORT || 3000;
+var app = express();
+
+var mysqlConnection = mysql.createConnection({
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6418563",
+  password: "iaQRShVh9g",
+  database: "sql6418563",
+  port: 3306
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'views')));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+app.get('/details', function(req, res) {
+    res.sendFile(path.join(__dirname+'/views/details.html'));
+});
+app.get('/customers', function (req, res) {
+    mysqlConnection.query('SELECT * from customers', (err, result) => {
+        res.status(200).send({customers: result})
+    })
+});
+
+app.get('/customers/:id', function (req, res) {
+    mysqlConnection.query('SELECT * from customers WHERE id = ?', [req.params.id], (err, result) => {
+        res.status(200).send({customer: result})
+    })
+});
+
+app.post('/transfer', function (req, res) {
+    const { fromid, toid, amount } = req.body;
+    mysqlConnection.query('SELECT * from customers WHERE id = ?', [fromid], (err, result) => {
+        if(result[0].amount >= amount) {
+            mysqlConnection.query('UPDATE customers SET amount = amount - ? WHERE id = ?', [amount, fromid], (err, result) => {
+                if(!err) {
+                    mysqlConnection.query('UPDATE customers SET amount = amount + ? WHERE id = ?', [amount, toid], (err, result) => {
+                        mysqlConnection.query('INSERT transaction_logs (from_id, to_id, amount) VALUES (?,?,?)', [fromid, toid, amount], (err, result) => {
+
+                        });
+                        res.status(200).send({error: false, message: 'Amount Transfered Successfully !'});
+                    });
+                }
+            });
+        }
+        else {
+            res.status(400).send({error: true, message: 'Your Bank balance amount is insufficient to transfer'});
+        }
+    })
+});
+
+app.get('/transferlogs/:id', function (req, res) {
+    mysqlConnection.query('SELECT * from transaction_logs WHERE from_id = ? OR to_id = ?', [req.params.id , req.params.id], (err, result) => {
+        res.status(200).send({logs: result})
+    })
+});
+app.listen(port, () => {
+    mysqlConnection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+});
---
+++ views/script.js
@@ -0,0 +1,123 @@
+
+var params;
+var currentCustomer;
+var otherCustomers;
+var SERVER_API_POINT = 'https://bankingappdemo.herokuapp.com/';
+function getCustomers() {
+    // Get All Custmers to show on home page
+    fetch(SERVER_API_POINT+'customers')
+    .then(response => response.json())
+    .then(data => {
+      let customers = data.customers;
+      let ct = document.getElementById('customersTable');
+        for( let i = 0; i < customers.length; i++) {
+            let newRow = ct.insertRow(i+1);
+            let nameCell = newRow.insertCell(0);
+            let actionCell = newRow.insertCell(1);
+            nameCell.innerHTML = customers[i].name;
+            actionCell.innerHTML = `
+                <button onclick="goToView(${customers[i].id})">View</button>
+            `
+        }
+    });
+}
+function goToView (customerId) {
+    window.location.href = '/details?id='+customerId
+}
+
+function getCustomerDetails () {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    params = Object.fromEntries(urlSearchParams.entries());
+    if(typeof params.id === 'undefined' || params.id === '' || isNaN(params.id)) {
+        window.location.href = "/";
+    } else {
+        // Call Api to fetch Current Contact details
+        fetch(SERVER_API_POINT+'customers/'+params.id)
+        .then(response => response.json())
+        .then(data => {
+            if (!data.customer[0]) window.location.href = "/";
+            currentCustomer = data.customer[0];
+            document.getElementById('customerName').innerHTML = data.customer[0].name;
+            document.getElementById('cName').innerHTML = data.customer[0].name+"'s Profile";
+            document.getElementById('customerAge').innerHTML = data.customer[0].age;
+            document.getElementById('customerBalance').innerHTML = '₹ '+data.customer[0].amount;
+            document.getElementById('customerEmail').innerHTML = data.customer[0].email;
+            // Get Available Customers
+            getAvailbleCustmers();
+        });
+    }
+}
+
+function getAvailbleCustmers() {
+    fetch(SERVER_API_POINT+'customers')
+        .then(response => response.json())
+        .then(data => {
+            otherCustomers = data.customers;
+            let dataList = document.getElementById('toContact');
+            dataList.innerHTML += `<option value=""> Select contact </option>`;
+            for(let i = 0 ; i < data.customers.length; i++) {
+                if(params.id != data.customers[i].id)
+                    dataList.innerHTML += `<option value="${data.customers[i].id}"> ${data.customers[i].name}</option>`
+            }
+            // Get Transfer Logs
+            getTransferLogs();
+        });
+}
+
+function getTransferLogs() {
+    fetch(SERVER_API_POINT+'transferlogs/'+params.id)
+        .then(response => response.json())
+        .then(data => {
+            let logsTable = document.getElementById('logsTable');
+            for(let i = 0 ; i < data.logs.length; i++) {
+                let status = data.logs[i].from_id == params.id ? 'Sent': 'Received';
+                let name ;
+                if (status === 'Sent') {
+                    name = otherCustomers.find((c) => c.id == data.logs[i].to_id).name;
+                } else {
+                    name = otherCustomers.find((c) => c.id == data.logs[i].from_id).name;
+                }
+
+                logsTable.innerHTML += `
+                    <div class="row">
+                        <div class="${status.toLowerCase()}">${status}</div>
+                        <div> ${status === 'Received' ? ' from ': ' to '} ${name}</div>
+                        <div>₹ ${data.logs[i].amount}</div>
+                        <div>${new Date(data.logs[i].created_at).toLocaleString()}</div>
+                    </div>`;
+            }
+        });
+}
+
+function transferAmount() {
+    let transferToContact = document.getElementById('toContact').value;
+    let transferAmount = document.getElementById('transferAmount').value;
+    if(transferAmount && transferToContact) {
+        // Call Api to transfer the amount
+        fetch(SERVER_API_POINT+'transfer', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                fromid: params.id,
+                toid: document.getElementById('toContact').value,
+                amount: transferAmount
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.error) {
+                alert(data.message);
+            } else {
+                window.location.href = '/';
+                setTimeout(() => {
+                    alert(data.message);
+                }, 2000);   
+            }
+        });
+    } else {
+        alert('Please provide Contact and Amount to transfer!!');
+    }
+}
